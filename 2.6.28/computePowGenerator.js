c1e1ddbfe7d1bf1cb3b566deebe17764d6b2f979
mehdi <habb.mehdi@gmail.com>
2022-09-14 22:23:08 -0400

    added timers for cycle duties

---
+++ 2.6.28/computePowGenerator.js
@@ -0,0 +1,42 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto = __importStar(require("@shardus/crypto-utils"));
+crypto.init('69fa4195670576c0160d660c3be36556ff8d504725be8a59b5a96509e0c994bc');
+function hex2bin(hex) {
+    let bin = '';
+    for (let i = 0; i < hex.length; i++) {
+        bin += parseInt(hex[i], 16).toString(2).padStart(4, '0');
+    }
+    return bin;
+}
+process.on('message', ({ seed, difficulty }) => {
+    let nonce, hash;
+    do {
+        nonce = crypto.randomBytes();
+        hash = crypto.hashObj({ seed, nonce });
+    } while (parseInt(hex2bin(hash).substring(0, difficulty), 2) !== 0);
+    process.send({
+        nonce,
+        hash,
+    });
+});
+//# sourceMappingURL=computePowGenerator.js.map
